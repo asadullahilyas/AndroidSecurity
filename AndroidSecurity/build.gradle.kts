f30f6c108ad244bfc63cdfc10e7490c2c1a537ff
Asadullah Ilyas <asadullah.ilyas@packagex.io>
2024-09-02 14:25:19 +0500

    Update build.gradle.kts

--- AndroidSecurity/build.gradle.kts
+++ AndroidSecurity/build.gradle.kts
@@ -49,7 +49,7 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("org.bouncycastle:bcpkix-jdk15on:1.67")
-    implementation("com.github.asadullahilyas:HandyUtils:1.0.0")
+    implementation("com.github.asadullahilyas:HandyUtils:1.0.6")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
